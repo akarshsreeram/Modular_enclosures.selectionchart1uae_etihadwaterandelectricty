5f12c9f264f8c5fdbf0c81a29d3d78c3491aa0f3
akarshsreeram <114436747+akarshsreeram@users.noreply.github.com>
2022-09-26 23:17:17 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='enclosure',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='HPC',
+    author_email='',
+    description=''
+)
